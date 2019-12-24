e59334fdf660b9a7c849b415fb42f53a7e9a90a6
Alexey_Gerasimov <web-47@mail.ru>
2019-12-25 00:24:18 +0300

    add buttons and h1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-    $( "#accordion" ).accordion();
+    $( '#accordion' ).accordion();
+    $('.but1').click(function(){
+        $('.sq1').animate({'width': '200px'}, 600);
+        $('.sq1 p').text('Свернуть');
+    });
+    $('.sq1').click(function(){
+        $(this).animate({'width': '40px'}, 600);
+        $('.sq1 p').empty();
+    });
+
 });
